f7f00c3dea794f5ce69ecac8d4d78f93fab24320
Iain <iain.hoolahan@gmail.com>
2022-11-22 14:21:12 +0000

    handling user input exercise complete

--- example_3/messageView.js
+++ example_3/messageView.js
@@ -12,11 +12,16 @@ class MessageView {
 
   displayMessage() {
     console.log('Thanks for clicking me!');
+
+    const message = document.querySelector('#message-input').value; // setting the message value to the input of the user
+
     const messageElement = document.createElement('div');
     messageElement.id = 'message';
-    messageElement.innerText = 'This is a message';
+    messageElement.innerText = message; 
 
-    document.querySelector('#main-container').append(messageElement);
+    document.querySelector('#main-container').append(messageElement); // posting the message to the page
+
+    document.querySelector('#message-input').value = ""; // empty the text box afetr being clicked
   }
 
   hideMessage() {
